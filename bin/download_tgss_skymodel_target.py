1fb751052bdc62f6decd102155117bf79fd1df14
Andreas Horneffer <AHorneffer@users.noreply.github.com>
2016-09-01 16:56:27 +0200

    reworked download_tgss_skymodel_target.py No dependence on astropy, the DoDownload parameter has now three levels.

--- bin/download_tgss_skymodel_target.py
+++ bin/download_tgss_skymodel_target.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os,sys
 import glob
-import re
 import pyrap.tables as pt
-#import pylab
-from astropy import units as u
-from astropy.coordinates import SkyCoord as sc
+import numpy as np
 
 
 ########################################################################
@@ -26,10 +23,11 @@ def grab_coo_MS(MS):
     
     # reading the coordinates ("position") from the MS
     # NB: they are given in rad,rad (J2000) 
-    [[[ra,dec]]] = pt.table(MS+'/FIELD', readonly=True, ack=False).getcol('PHASE_DIR')
+    [[[ra,dec]]] = pt.table(MS+'::FIELD', readonly=True, ack=False).getcol('PHASE_DIR')
     
-    # for a small conversion to degrees for the tgss query
-    coo_tar=sc(ra,dec,unit=(u.rad, u.rad),frame="icrs")
+    # convert radians to degrees
+    ra_deg =  ra/np.pi*180.
+    dec_deg = dec/np.pi*180.
     
     # and sending the coordinates in deg
     return coo_tar.ra.deg,coo_tar.dec.deg
@@ -55,7 +53,7 @@ def input2strlist_nomapfile(invar):
 
     
 ########################################################################
-def main(ms_input,SkymodelTar,Radius,Yes="True"):
+def main(ms_input, SkymodelPath, Radius="5.", DoDownload="True"):
     """
     Download the TGSS skymodel for the target field
 
@@ -63,27 +61,53 @@ def main(ms_input,SkymodelTar,Radius,Yes="True"):
     ----------
     ms_input : str
         String from the list (map) of the target MSs
-    SkymodelTar : str
+    SkymodelPath : str
         Full name (with path) to the skymodel; if YES is true, the TGSS skymodel will be downloaded here
-    Radius : float
+    Radius : string with float (default = "5.")
         Radius for the TGSS cone search in degrees
-    YES : str ("True" or "False")
+    DoDownload : str ("Force" or "True" or "False")
         Download or not the TGSS skymodel.
-        (If False, one supposes the pipeline will use the skymodel provided in SkymodelTar)
+        "Force": download skymodel from TGSS, delete existing skymodel if needed.
+        "True" or "Yes": use existing skymodel file if it exists, download skymodel from 
+                         TGSS if it does not.
+        "False" or "No": Do not download skymodel, raise an exception if skymodel
+                         file does not exist.
     
     """
     
-    if Yes=="True":# [TODO: (How to) send directly a bolean)
-        print "DOWNLOADING TGSS Skymodel for the target into "+ SkymodelTar
+    FileExists = os.path.isfile(SkymodelPath)
+    if (not FileExists and os.path.exists(SkymodelPath)):
+        raise ValueError("download_tgss_skymodel_target: WTF! Path: \"%s\" exists but is not a file!"%(SkymodelPath))
+    download_flag = False
+    if DoDownload.upper() == "FORCE":
+        if FileExists:
+            os.remove(SkymodelPath)
+        download_flag = True
+    elif DoDownload.upper() == "TRUE" or DoDownload.upper() == "YES":
+        if FileExists:
+            print "USING the exising skymodel in "+ SkymodelPath
+            return
+        else:
+            download_flag = True
+    elif DoDownload.upper() == "False" or DoDownload.upper() == "NO":
+         if FileExists:
+            print "USING the exising skymodel in "+ SkymodelPath
+            return
+        else:
+            raise ValueError("download_tgss_skymodel_target: Path: \"%s\" does not exist and TGSS download is disabled!"%(SkymodelPath))
+
+    # If we got here, then we are supposed to download the skymodel.
+    assert download_flag == True # Jaja, belts and suspenders...
+    print "DOWNLOADING TGSS Skymodel for the target into "+ SkymodelPath
+    
+    # Reading a MS to find the coordinate (pyrap)
+    [RATar,DECTar]=grab_coo_MS(input2strlist_nomapfile(ms_input)[0])
         
-        # Reading a MS to find the coordinate (pyrap)
-        [RATar,DECTar]=grab_coo_MS(input2strlist_nomapfile(ms_input)[0])
-        
-        # Downloading the skymodel
-        os.system("wget -O "+SkymodelTar+ " \'http://tgssadr.strw.leidenuniv.nl/cgi-bin/gsmv2.cgi?coord="+str(RATar)+","+str(DECTar)+"&radius="+Radius+"&unit=deg&deconv=y\' ")
-    else:
-        print "USING the skymodel provided in "+ SkymodelTar
-        
+    # Downloading the skymodel
+    os.system("wget -O "+SkymodelPath+ " \'http://tgssadr.strw.leidenuniv.nl/cgi-bin/gsmv2.cgi?coord="+str(RATar)+","+str(DECTar)+"&radius="+Radius+"&unit=deg&deconv=y\' ")
+
+    return
+            
     
 ########################################################################
 if __name__ == '__main__':
@@ -96,8 +120,6 @@ if __name__ == '__main__':
                         help='Full name (with path) to the skymodel; the TGSS skymodel will be downloaded here')
     parser.add_argument('--Radius', type=float, 
                         help='Radius for the TGSS cone search in degrees')
-        
- 
    
     args = parser.parse_args()
     radius=5
