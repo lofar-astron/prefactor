df40fc5887bd65b3cf38fe3a7f63bdcbd37d5e69
Andreas Horneffer <AHorneffer@users.noreply.github.com>
2017-07-04 17:27:52 +0200

    More robust fix for an issue that showed up in PILL See: https://github.com/lofar-astron/PiLL/issues/21

--- scripts/sort_times_into_freqGroups.py
+++ scripts/sort_times_into_freqGroups.py
@@ -204,8 +204,8 @@ def main(ms_input, filename=None, mapfile_dir=None, numSB=-1, hosts=None, NDPPPf
     freq_width = np.min(freqliste[1:]-freqliste[:-1])
     if file_bandwidth > freq_width:
         raise ValueError("Bandwidth of files is larger than minimum frequency step between two files!")
-    if file_bandwidth < (freq_width/2.):
-        raise ValueError("Bandwidth of files is smaller than half the minimum frequency step between two files! (More than half the data is missing.)")
+    if file_bandwidth < (freq_width*0.51):
+        raise ValueError("Bandwidth of files is smaller than 51% of the minimum frequency step between two files! (More than about half the data is missing.)")
     #the new output map
     filemap = MultiDataMap()
     groupmap = DataMap()
