5d3a9f1e62d99c4f8d383292c3370ce669be594b
Alexander Drabent <drabent@lof007.control.lofar>
2019-08-29 07:48:10 +0000

    fix SkymodelName.mapfile error, #248

--- scripts/find_skymodel_cal.py
+++ scripts/find_skymodel_cal.py
@@ -72,11 +72,7 @@ def find_skymodel(ra, dec, PathSkyMod, extensionSky = ".skymodel", max_separatio
     
 
     skymodels = glob.glob(PathSkyMod + "/*" + extensionSky)
-    
-    # remove any Ateam models from the listing (only in filenames)
-    #skymodels = [s for s in skymodels if 'Ateam' not in s]
-    #skymodels = [s for s in skymodels if 'A-Team' not in s]
-    
+        
     for skymodel in skymodels:
         check = check_skymodel(skymodel, ra, dec, max_separation_arcmin)
         if check[0]:
@@ -134,7 +130,7 @@ def main(ms_input, DirSkymodelCal, extensionSky=".skymodel", max_separation_arcm
 
     if os.path.isfile(DirSkymodelCal):
         print "Using the skymodel provided: " + DirSkymodelCal
-        return { 'SkymodelCal' : DirSkymodelCal }
+        return { 'SkymodelCal' : DirSkymodelCal, 'SkymodelName': os.path.splitext(os.path.basename(DirSkymodelCal))[0] }
         
     elif os.path.isdir(DirSkymodelCal):
         ra, dec = grab_pointing(input2strlist_nomapfile(ms_input)[0])
