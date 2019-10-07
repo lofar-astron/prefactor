b434db0b02167e67746d184c22a964f4452a6280
Alexander Drabent <drabent@lof009.control.lofar>
2019-10-07 13:00:35 +0000

    optimize formatting of summary

--- scripts/make_summary.py
+++ scripts/make_summary.py
@@ -163,14 +163,14 @@ def main(observation_directory = '/data/share/pipeline/Observation', logfile = '
 		soltab   = solset.getSoltab(soltab_name)
 		antennas = soltab.ant
 		axes     = soltab.getAxesNames()
-		axes.insert(0, axes.pop(axes.index('ant'))) ## put antenna table to the second place
+		axes.insert(0, axes.pop(axes.index('ant')))
 		flagged_solutions[soltab_name] = {}
 		for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=axes, weight=True):
 			vals    = losoto.reorderAxes(vals,    soltab.getAxesNames(), axes)
 			weights = losoto.reorderAxes(weights, soltab.getAxesNames(), axes)
 		for i, antenna in enumerate(antennas):
 			flagged_fraction = 1. - float(numpy.mean(weights[i]))
-			flagged_solutions[soltab_name][antenna] = soltab_len.format('{:.2f}'.format(100 * flagged_fraction) + '%')
+			flagged_solutions[soltab_name][antenna] = soltab_len.format('{: 6.2f}'.format(100 * flagged_fraction) + '%')
 	soltab_names = ' '.join([ soltab_len.format(soltab_name) for soltab_name in soltabs ])
 	f_summary.write('Amount of flagged solutions per station and solution table:\n')
 	f_summary.write(antenna_len.format('Station') + ' ' + soltab_names + '\n')
