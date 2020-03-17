cd7c12d596de54956c7cf351ecd28ca0ba7cddda
Stefano Alberto Russo <stefano.russo@gmail.com>
2020-03-17 16:22:53 +0100

    Fixes for Python3 compatibility

--- plugins/PipelineStep_makeDirectory.py
+++ plugins/PipelineStep_makeDirectory.py
@@ -22,10 +22,10 @@ def plugin_main(args, **kwargs):
 
     if os.path.exists(dir_to_create):
         if not os.path.isdir(dir_to_create):
-            print "Plugin makeDirectory: Path {} exists, but is no directory!".format(dir_to_create)
-            raise ValueError, "Path {} exists, but is no directory!".format(dir_to_create)
+            print("Plugin makeDirectory: Path {} exists, but is no directory!".format(dir_to_create))
+            raise ValueError("Path {} exists, but is no directory!".format(dir_to_create))
     else:
-        os.makedirs(dir_to_create,mode=0755)
+        os.makedirs(dir_to_create,mode=0o755)
 
     result = {}
     return result
