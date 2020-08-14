8f07bd0b059ceb0165540361eeca2717f53fb837
Alexander Drabent <drabent@lof009.control.lofar>
2020-08-14 13:48:44 +0000

    add check for decoding

--- scripts/createRMh5parm.py
+++ scripts/createRMh5parm.py
@@ -178,7 +178,10 @@ def main(MSfiles, h5parmdb, solset_name = "sol000",timestepRM=300,
             raise ValueError("Couldn't get RM information from RMextract! (But I don't know why.)")
         
     logging.info('Adding rotation measure values to: ' + solset_name + ' of ' + h5parmdb)
-    rm_vals=np.array([rmdict["RM"][stat.decode()].flatten() for stat in station_names])
+    if type(station_names[0]) != str:
+        rm_vals = np.array([rmdict["RM"][stat.decode()].flatten() for stat in station_names])
+    else:
+        rm_vals = np.array([rmdict["RM"][stat].flatten() for stat in station_names])
     new_soltab = solset.makeSoltab(soltype='rotationmeasure', soltabName='RMextract',
                                    axesNames=['ant', 'time'], axesVals=[station_names, rmdict['times']],
                                    vals=rm_vals,
